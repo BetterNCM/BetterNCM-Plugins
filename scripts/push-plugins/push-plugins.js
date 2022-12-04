672a4129141c3d5ec020e269f8da869b5a3841ed
solstice23 <solstice233@gmail.com>
2022-12-04 19:25:27 +0800

    Add push script

--- scripts/push-plugins/push-plugins.js
+++ scripts/push-plugins/push-plugins.js
@@ -7,10 +7,10 @@ execSync("git config --global user.email 'betterncm-bot@proton.me'");
 
 
 const tmpPath = path.resolve(process.cwd(), '../../tmp');
-const rootPath = path.resolve(process.cwd(), '../../../');
+const rootPath = path.resolve(process.cwd(), '../../');
 const pluginsPath = path.resolve(rootPath, 'BetterNCM-Packed-Plugins');
 
 const repo = `https://BetterNCM-Bot:${process.env.GITHUB_TOKEN}@github.com/BetterNCM/BetterNCM-Packed-Plugins.git`;
-execSync(`git clone --single-branch --depth 1 --branch "master" "${repo}"`, { cwd: '/' });
+execSync(`git clone --single-branch --depth 1 --branch "master" "${repo}"`, { cwd: rootPath });
 
 fs_extra.copySync(tmpPath, pluginsPath, { overwrite: false });
