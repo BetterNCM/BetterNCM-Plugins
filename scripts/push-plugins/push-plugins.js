4d45caa03cc2d652ef076c68244f35e9a637c1d8
solstice23 <solstice233@gmail.com>
2022-12-04 16:30:59 +0800

    Add push script

--- scripts/push-plugins/push-plugins.js
+++ scripts/push-plugins/push-plugins.js
@@ -4,11 +4,12 @@ import { execSync } from 'child_process';
 
 execSync("git config --global user.name 'BetterNCM-Bot'");
 execSync("git config --global user.email 'betterncm-bot@proton.me'");
+execSync("git config --global credential.helper cache");
 
 const tmpPath = path.resolve(process.cwd(), '../../tmp');
 const rootPath = path.resolve(process.cwd(), '../../../');
 const pluginsPath = path.resolve(rootPath, 'BetterNCM-Packed-Plugins');
-execSync('git clone https://github.com/BetterNCM/BetterNCM-Packed-Plugins.git', { cwd: rootPath });
+execSync(`git clone https://${process.env.GITHUB_TOKEN}}@github.com/BetterNCM/BetterNCM-Packed-Plugins.git`, { cwd: rootPath });
 execSync('git remote set-url origin git@github.com:BetterNCM/BetterNCM-Packed-Plugins.git', { cwd: pluginsPath });
 fs_extra.copySync(tmpPath, pluginsPath, { overwrite: false });
 execSync('git add .', { cwd: pluginsPath });
