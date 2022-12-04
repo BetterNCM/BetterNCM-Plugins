1d5d0bd8470e0c63ea06b540e2d11414adba4ca7
solstice23 <solstice233@gmail.com>
2022-12-04 15:55:30 +0800

    Add push script

--- scripts/push-plugins/push-plugins.js
+++ scripts/push-plugins/push-plugins.js
@@ -9,7 +9,8 @@ const tmpPath = path.resolve(process.cwd(), '../../tmp');
 const rootPath = path.resolve(process.cwd(), '../../../');
 execSync('git clone https://github.com/BetterNCM/BetterNCM-Packed-Plugins.git', { cwd: rootPath });
 fs_extra.copySync(tmpPath, path.resolve(rootPath, 'BetterNCM-Packed-Plugins'), { overwrite: false });
-execSync('git add .', { cwd: rootPath + '/BetterNCM-Packed-Plugins' });
+/*execSync('git add .', { cwd: rootPath + '/BetterNCM-Packed-Plugins' });
 execSync('git commit -m "update"', { cwd: rootPath + '/BetterNCM-Packed-Plugins' });
 execSync('git push', { cwd: rootPath + '/BetterNCM-Packed-Plugins' });
 
+*/
