a060fa442b6ea40375d53aa461b6c0a66e8aae86
solstice23 <solstice233@gmail.com>
2022-12-04 19:07:50 +0800

    Add push script

--- scripts/push-plugins/push-plugins.js
+++ scripts/push-plugins/push-plugins.js
@@ -4,14 +4,17 @@ import { execSync } from 'child_process';
 
 execSync("git config --global user.name 'BetterNCM-Bot'");
 execSync("git config --global user.email 'betterncm-bot@proton.me'");
-execSync("git config --global credential.helper cache");
+
 
 const tmpPath = path.resolve(process.cwd(), '../../tmp');
 const rootPath = path.resolve(process.cwd(), '../../../');
 const pluginsPath = path.resolve(rootPath, 'BetterNCM-Packed-Plugins');
-execSync(`git clone https://${process.env.GITHUB_TOKEN}@github.com/BetterNCM/BetterNCM-Packed-Plugins.git`, { cwd: rootPath });
-execSync(`git remote set-url origin https://${process.env.GITHUB_TOKEN}@github.com/BetterNCM/BetterNCM-Packed-Plugins`, { cwd: pluginsPath });
+
+
+const repo = `https://BetterNCM-Bot:${process.env.GITHUB_TOKEN}@github.com/BetterNCM/BetterNCM-Packed-Plugins.git`;
+
+execSync(`git clone --single-branch --depth 1 --branch "master" "${repo}"`, { cwd: rootPath });
 fs_extra.copySync(tmpPath, pluginsPath, { overwrite: false });
 execSync('git add .', { cwd: pluginsPath });
 execSync('git commit -m "update"', { cwd: pluginsPath });
-execSync('git push', { cwd: pluginsPath });
+execSync(`git push "${repo}"`, { cwd: pluginsPath });
