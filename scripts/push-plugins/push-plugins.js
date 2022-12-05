1314ed156efafbe0455c1d304ddd33df9297fae4
solstice23 <solstice233@gmail.com>
2022-12-05 11:33:14 +0800

    Add push script

--- scripts/push-plugins/push-plugins.js
+++ scripts/push-plugins/push-plugins.js
@@ -2,10 +2,6 @@ import fs_extra from 'fs-extra';
 import path from 'path';
 import { execSync } from 'child_process';
 
-execSync("git config --global user.name 'BetterNCM-Bot'");
-execSync("git config --global user.email 'betterncm-bot@proton.me'");
-
-
 const tmpPath = path.resolve(process.cwd(), '../../tmp');
 const rootPath = path.resolve(process.cwd(), '../../');
 const pluginsPath = path.resolve(rootPath, 'BetterNCM-Packed-Plugins');
