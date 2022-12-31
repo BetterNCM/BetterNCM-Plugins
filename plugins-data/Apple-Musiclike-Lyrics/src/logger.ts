6862c0ced6f93f1ac035acd31957d6dde2fceffe
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2022-12-31 22:41:26 +0800

    Delete plugins-data/Apple-Musiclike-Lyrics directory

--- plugins-data/Apple-Musiclike-Lyrics/src/logger.ts
+++
@@ -1,9 +0,0 @@
-const noop = () => {}
-
-export const debug = DEBUG ? console.debug : noop;
-
-export const log = DEBUG ? console.log : noop;
-
-export const warn = DEBUG ? console.warn : noop;
-
-export const error = DEBUG ? console.error : noop;
