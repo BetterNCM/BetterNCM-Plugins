b8fed66126bb094fd85e4603b6d34ea12b7d3165
solstice23 <solstice233@gmail.com>
2023-02-21 20:23:07 +0800

    Update pack-plugins.js

--- scripts/pack-plugins/pack-plugins.js
+++ scripts/pack-plugins/pack-plugins.js
@@ -105,7 +105,10 @@ plugins.forEach((plugin) => {
 	console.log(`📦 ${slug} ${manifest.version} packed.`);
 });
 
+// temporary
 const allThemes = pluginList.filter(plugin=>plugin['type']==='theme').map(plugin=>plugin.slug)
-const allJson = JSON.stringify(pluginList.map(plugin=>( { ...plugin, type: 'framework', incompatible: [ ...plugin.incompatible??[], ...(plugin.type==='theme'?allThemes:[]) ] } )), null, 4);
+pluginList = pluginList.map(plugin=>( { ...plugin, type: 'framework', incompatible: [ ...plugin.incompatible??[], ...(plugin.type==='theme'?allThemes.filter(theme=>theme.slug!=plugin.slug):[]) ] } ))
+
+const allJson = JSON.stringify(pluginList, null, 4);
 fs.writeFileSync(path.resolve(process.cwd(), '../../tmp/plugins.json'), allJson);
 console.log('\n✅ All plugins packed.');
