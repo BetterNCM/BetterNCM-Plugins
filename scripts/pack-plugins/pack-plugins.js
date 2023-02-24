cb739ba361e89fc7844308986a5531ea11360839
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2023-02-24 13:22:27 +0800

    Update pack-plugins.js

--- scripts/pack-plugins/pack-plugins.js
+++ scripts/pack-plugins/pack-plugins.js
@@ -105,10 +105,6 @@ plugins.forEach((plugin) => {
 	console.log(`📦 ${slug} ${manifest.version} packed.`);
 });
 
-// temporary
-const allThemes = pluginList.filter(plugin=>plugin['type']==='theme').map(plugin=>plugin.slug)
-pluginList = pluginList.map(plugin=>( { ...plugin, type: 'framework', incompatible: [ ...plugin.incompatible??[], ...(plugin.type==='theme'?allThemes.filter(theme=>theme!=plugin.slug):[]) ] } ))
-
 const allJson = JSON.stringify(pluginList, null, 4);
 fs.writeFileSync(path.resolve(process.cwd(), '../../tmp/plugins.json'), allJson);
 console.log('\n✅ All plugins packed.');
