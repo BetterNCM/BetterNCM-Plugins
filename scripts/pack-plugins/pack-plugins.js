165293963b1a901b9c4c04198ff45a9d8f0468cd
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2023-03-31 23:32:47 +0800

    feat: check size before packing

--- scripts/pack-plugins/pack-plugins.js
+++ scripts/pack-plugins/pack-plugins.js
@@ -37,85 +37,87 @@ fs.mkdirSync(tmpPath);
 fs.mkdirSync(path.resolve(tmpPath, 'plugins'));
 fs.mkdirSync(path.resolve(tmpPath, 'previews'));
 
+!(async()=>{
 
-let pluginList = [];
+	let pluginList = [];
 
-const plugins = fs.readdirSync(path.resolve(process.cwd(), '../../plugins-data'));
-plugins.forEach((plugin) => {
-	if (plugin.startsWith('.')) return;
-	if (!fs.existsSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/manifest.json`))) {
-		console.log(`❌ Plugin ${plugin} has no manifest.json.`);
-		return;
-	}
-	const manifest = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/manifest.json`)));
-	
-	let pluginJson = {};
-	for (const field of compulsoryFields) {
-		addField(pluginJson, field, manifest[field]);
-	}
-	if (!checkCompulsoryFields(pluginJson, plugin)) {
-		console.log(`⏩ Packing skipped.`);
-		return;
-	}
-	for (const field of optionalFields) {
-		addField(pluginJson, field, manifest[field]);
-	}
-	const slug = manifest?.slug ?? getSlugName(manifest.name);
-	addField(pluginJson, 'slug', slug);
-	addField(pluginJson, 'update_time', parseInt(execSync(`git log -1 --format=%ct ${path.resolve(process.cwd(), `../../plugins-data/${plugin}/manifest.json`)}`)));
-	addField(pluginJson, 'publish_time', parseInt(execSync(`git log --reverse --format=%ct ${path.resolve(process.cwd(), `../../plugins-data/${plugin}/manifest.json`)}`).toString().split('\n')[0]));
-	const repo = definedPluginList.find((definedPlugin) => definedPlugin.slug === plugin)?.repo ?? '';
-	addField(pluginJson, 'repo', repo);
-	addField(pluginJson, 'stars', starCount[repo] ?? 0);
-
-	try {
-		const repo = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), `../../plugins-list/${slug}.json`))).repo;
-		addField(pluginJson, 'repo', repo);
-	} catch {}
-	
-	if (pluginJson.preview) {
-		pluginJson.preview = pluginJson.preview.replace(/^\.?[\\/]/g, '');
-		if (!fs.existsSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/${pluginJson.preview}`))) {
-			console.log(`🖼️ Preview of ${plugin} not found, ignored.`);
-			delete pluginJson.preview;
-		}else{
-			const suffix = pluginJson.preview.split('.').pop();
-			fs.copyFileSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/${pluginJson.preview}`), path.resolve(tmpPath, 'previews', `${slug}.${suffix}`));
-			fs.rmSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/${pluginJson.preview}`));
-			pluginJson.preview = `previews/${slug}.${suffix}`;
+	const plugins = fs.readdirSync(path.resolve(process.cwd(), '../../plugins-data'));
+	for(const plugin of plugins){
+		if (plugin.startsWith('.')) return;
+		if (!fs.existsSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/manifest.json`))) {
+			console.log(`❌ Plugin ${plugin} has no manifest.json.`);
+			return;
 		}
+		const manifest = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/manifest.json`)));
+
+		let pluginJson = {};
+		for (const field of compulsoryFields) {
+			addField(pluginJson, field, manifest[field]);
+		}
+		if (!checkCompulsoryFields(pluginJson, plugin)) {
+			console.log(`⏩ Packing skipped.`);
+			return;
+		}
+		for (const field of optionalFields) {
+			addField(pluginJson, field, manifest[field]);
+		}
+		const slug = manifest?.slug ?? getSlugName(manifest.name);
+		addField(pluginJson, 'slug', slug);
+		addField(pluginJson, 'update_time', parseInt(execSync(`git log -1 --format=%ct ${path.resolve(process.cwd(), `../../plugins-data/${plugin}/manifest.json`)}`)));
+		addField(pluginJson, 'publish_time', parseInt(execSync(`git log --reverse --format=%ct ${path.resolve(process.cwd(), `../../plugins-data/${plugin}/manifest.json`)}`).toString().split('\n')[0]));
+		const repo = definedPluginList.find((definedPlugin) => definedPlugin.slug === plugin)?.repo ?? '';
+		addField(pluginJson, 'repo', repo);
+		addField(pluginJson, 'stars', starCount[repo] ?? 0);
+
+		try {
+			const repo = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), `../../plugins-list/${slug}.json`))).repo;
+			addField(pluginJson, 'repo', repo);
+		} catch {}
+
+		if (pluginJson.preview) {
+			pluginJson.preview = pluginJson.preview.replace(/^\.?[\\/]/g, '');
+			if (!fs.existsSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/${pluginJson.preview}`))) {
+				console.log(`🖼️ Preview of ${plugin} not found, ignored.`);
+				delete pluginJson.preview;
+			}else{
+				const suffix = pluginJson.preview.split('.').pop();
+				fs.copyFileSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/${pluginJson.preview}`), path.resolve(tmpPath, 'previews', `${slug}.${suffix}`));
+				fs.rmSync(path.resolve(process.cwd(), `../../plugins-data/${plugin}/${pluginJson.preview}`));
+				pluginJson.preview = `previews/${slug}.${suffix}`;
+			}
+		}
+
+		if (manifest.native_plugin) {
+			addField(pluginJson, 'native', true);
+		}
+		if (manifest.require_restart) {
+			addField(pluginJson, 'require_restart', true);
+		}
+
+
+		const pluginPath = path.resolve(process.cwd(), `../../tmp/plugins/${slug}-${manifest.version}.plugin`);
+		await compressing.zip.compressDir(path.resolve(process.cwd(), `../../plugins-data/${plugin}`), pluginPath, {
+			ignoreBase: true
+		});
+
+		const stats = fs.statSync(pluginPath);
+		const fileSizeInBytes = stats.size;
+		const fileSizeInKiB = fileSizeInBytes / 1024;
+		if (fileSizeInKiB > 800) {
+		  console.warn('⚠ 插件文件大于 800KiB，正在跳过');
+		  console.warn(`📦 ${slug} ${manifest.version} skipped.`);
+		} else if (fileSizeInKiB > 600) {
+		  console.warn('⚠ 插件文件大于 600KiB');
+		}
+
+		addField(pluginJson, 'file', `${slug}-${manifest.version}.plugin`);
+		addField(pluginJson, 'file-url', `plugins/${slug}-${manifest.version}.plugin`);
+
+		pluginList.push(pluginJson);
+		console.log(`📦 ${slug} ${manifest.version} packed.`);
 	}
 
-	if (manifest.native_plugin) {
-		addField(pluginJson, 'native', true);
-	}
-	if (manifest.require_restart) {
-		addField(pluginJson, 'require_restart', true);
-	}
-
-
-	const pluginPath = path.resolve(process.cwd(), `../../tmp/plugins/${slug}-${manifest.version}.plugin`);
-	compressing.zip.compressDir(path.resolve(process.cwd(), `../../plugins-data/${plugin}`), pluginPath, {
-		ignoreBase: true
-	});
-	
-	const stats = fs.statSync(pluginPath);
-	const fileSizeInBytes = stats.size;
-	const fileSizeInKiB = fileSizeInBytes / 1024;
-	if (fileSizeInKiB > 800) {
-	  console.warn('⚠ 插件文件大于 800KiB，正在跳过');
-	  console.warn(`📦 ${slug} ${manifest.version} skipped.`);
-	} else if (fileSizeInKiB > 600) {
-	  console.warn('⚠ 插件文件大于 600KiB');
-	}
-
-	addField(pluginJson, 'file', `${slug}-${manifest.version}.plugin`);
-	addField(pluginJson, 'file-url', `plugins/${slug}-${manifest.version}.plugin`);
-	
-	pluginList.push(pluginJson);
-	console.log(`📦 ${slug} ${manifest.version} packed.`);
-});
-
-const allJson = JSON.stringify(pluginList, null, 4);
-fs.writeFileSync(path.resolve(process.cwd(), '../../tmp/plugins.json'), allJson);
-console.log('\n✅ All plugins packed.');
+	const allJson = JSON.stringify(pluginList, null, 4);
+	fs.writeFileSync(path.resolve(process.cwd(), '../../tmp/plugins.json'), allJson);
+	console.log('\n✅ All plugins packed.');
+})()
